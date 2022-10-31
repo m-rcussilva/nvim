7e933f9a258e5fe5fb5cd1d6580aa920e2798f71
Marcus Silva <marcusluis.mail@gmail.com>
2022-10-31 14:20:33 -0300

    how to change the tux in lualine for another icon?

--- lua/ms/plugins/lualine.lua
+++ lua/ms/plugins/lualine.lua
@@ -3,4 +3,10 @@ if not status then
     return
 end
 
-lualine.setup()
+lualine.setup({
+    options = {
+        icons_enabled = true,
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+    },
+})
